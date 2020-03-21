ae5a416ec5dd8d2f7bc0de71e29a98d38252e6c0
Brandon <brandon.claybourn@my.jcu.edu.au>
2020-03-21 16:26:49 +1000

    added inadequate security checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,11 +1,19 @@
+#1.
+# numbers = []
+# while len(numbers) < 5:
+#     numbers.append(int(input("Number: ")))
+#
+# print("The first number is {}".format(numbers[0]))
+# print("The last number is {}".format(numbers[-1]))
+# print("The smallest number is {}".format(min(numbers)))
+# print("The largest number is {}".format(max(numbers)))
+# print("The average of the numbers is {}".format(sum(numbers) / len(numbers)))
 
-numbers = []
-while len(numbers) < 5:
-    numbers.append(int(input("Number: ")))
+#2.
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
-print("The first number is {}".format(numbers[0]))
-print("The last number is {}".format(numbers[-1]))
-print("The smallest number is {}".format(min(numbers)))
-print("The largest number is {}".format(max(numbers)))
-print("The average of the numbers is {}".format(sum(numbers) / len(numbers)))
-
+username_check = input("Enter username:  ")
+if username_check in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
