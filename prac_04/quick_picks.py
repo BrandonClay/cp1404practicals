8eb4c9f95dc98ae96641d5b372fbe03fd074bc36
Brandon <brandon.claybourn@my.jcu.edu.au>
2020-03-22 11:10:27 +1000

    added attempt of quick_picks

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,15 @@
+import random
+
+line_size = 6
+minimum = 1
+maximum = 45
+
+quick_picks = []
+number_of_picks = int(input("How many quick picks? "))
+
+for i in range (number_of_picks):
+    for j in range (line_size):
+        number = random.randint(minimum, maximum )
+        quick_picks.append(number)
+    print(quick_picks)
+#struggled with working this one out sorry
