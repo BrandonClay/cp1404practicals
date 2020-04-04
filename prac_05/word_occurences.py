7715a0f70ebc3898fa897c9d6d54cf93fb4978ff
Brandon <brandon.claybourn@my.jcu.edu.au>
2020-04-05 03:42:24 +1000

    added sort, max length formatting for printing the list.

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -8,8 +8,9 @@ for word in word_list:
     frequency = words_counting.get(word, 0)
     words_counting[word] = frequency + 1
 
+max_variable = max(len(word)for word in word_list)
+word_list = list(words_counting.keys())
+word_list.sort()
+
 for word in word_list:
-    print(word, words_counting[word])
-
-
-print(words_counting)
+    print("{:{}}: {}".format(word, max_variable, words_counting[word]))
