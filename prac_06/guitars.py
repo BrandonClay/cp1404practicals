50db75f364f2eb9dfc25b4d5d7162ff1cdf380cd
Brandon <brandon.claybourn@my.jcu.edu.au>
2020-04-18 13:33:06 +1000

    added intermediate and do-from-scratch exercises.

---
+++ prac_06/guitars.py
@@ -0,0 +1,33 @@
+from prac_06.guitar import Guitar
+
+
+def main():
+    guitars = []
+
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $ "))
+        guitars.append(Guitar(name, year, cost))
+        print(Guitar(name, year, cost), "added.")
+        name = input("Name: ")
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    if guitars:
+        i = 0
+        print("These are my guitars: ")
+        for guitar in guitars:
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = "(vintage)"
+                print("Guitar {}: {} ({}), worth ${:5,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost,
+                                                                     vintage_string))
+                i = i + 1
+            else:
+                print(("Guitar {}: {} ({}), worth ${:5,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost,
+                                                                      vintage_string)))
+
+
+main()
